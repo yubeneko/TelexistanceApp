6d12fd38877314431394b4c58f8bd26834d3c33f
yubeneko <ryohei2674@gmail.com>
2019-09-08 15:37:42 +0900

    UDP部分実装完了

---
+++ Assets/TelexistanceApp/Scripts/UdpSender.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Net.Sockets;
+
+public class UdpSender : MonoBehaviour
+{
+    public string remoteHost = "192.168.10.86";
+    public int remotePort = 60000;
+    
+    UdpClient udpClient;
+
+    void Start()
+    {
+        udpClient = new UdpClient();
+
+        SendData("hello");
+        SendData("Oh.");
+    }
+
+    void SendData (string data)
+    {
+        byte[] sendBytes = System.Text.Encoding.ASCII.GetBytes(data);
+
+        try
+        {
+            udpClient.Send(sendBytes, sendBytes.Length, remoteHost, remotePort);
+            Debug.Log("Data was sent.");
+        }
+        catch (SocketException se)
+        {
+            Debug.LogError(se.ToString());
+            Debug.LogError($"Error Code : {se.ErrorCode}");
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        udpClient.Close();
+        Debug.Log("udp was closed.");
+    }
+}
